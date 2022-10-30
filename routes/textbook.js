335e6d7856754ae1a6d306624167c35baaf931f8
swbae1206 <swbae77@naver.com>
2022-10-30 20:43:12 +0900

    commit

--- routes/textbook.js
+++ routes/textbook.js
@@ -7,6 +7,10 @@ router.get('/html', function (req, res) {
 	res.sendFile(__dirname + "/html/textbook/html.html");
 })
 
+router.get('/css', function (req, res) {
+	res.sendFile(__dirname + "/html/textbook/css.html");
+})
+
 
 module.exports = router;
 
